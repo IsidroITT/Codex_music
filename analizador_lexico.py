50c54715656285f6b577291be45c37dc6bd15680
Isidro-TEC <isanfloresfa@ittepic.edu.mx>
2023-09-25 01:36:32 -0700

    Sin codigo viejo

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -5,18 +5,6 @@ resultado_lexema = []
 
 reservada = (
     # Palabras Reservadas
-#     'INCLUDE',
-#     'USING',
-#     'NAMESPACE',
-#     'STD',
-#     'COUT',
-#     'CIN',
-#    'GET',
-#    'CADENA',
-#   'RETURN',
-#    'VOID',
-#     'INT',
-#     'ENDL',
     'inicio','INICIO',
     'fin','FIN',
     'tempo','TEMPO',
@@ -27,52 +15,6 @@ reservada = (
     'pianoserv','PIANOSERV', 
 )
 tokens = reservada + (
-#     'IDENTIFICADOR',
-#     'ENTERO',
-#     'ASIGNAR',
-
-#     'SUMA',
-#     'RESTA',
-#     'MULT',
-#     'DIV',
-#     'POTENCIA',
-#     'MODULO',
-
-#    'MINUSMINUS',
-#    'PLUSPLUS',
-
-#     #Condiones
-#    'SI',
-#     'SINO',
-#     #Ciclos
-#    'MIENTRAS',
-#    'PARA',
-#     #logica
-#     'AND',
-#     'OR',
-#     'NOT',
-#     'MENORQUE',
-#     'MENORIGUAL',
-#     'MAYORQUE',
-#     'MAYORIGUAL',
-#     'IGUAL',
-#     'DISTINTO',
-#     # Symbolos
-#     'NUMERAL',
-
-#     'PARIZQ',
-#     'PARDER',
-#     'CORIZQ',
-#     'CORDER',
-#     'LLAIZQ',
-#     'LLADER',
-    
-#     # Otros
-#     'PUNTOCOMA',
-#     'COMA',
-#     'COMDOB',
-#     'MAYORDER', #>>
-#     'MAYORIZQ', #<<
      'DIGITO',
      'NOTA',
      'PUNTILLO',
@@ -122,20 +64,6 @@ t_BEMOL = r'b'
 t_CLAVE_COMPAS = r'[A-G]\^[1-4]'
 t_INICIO_PARTITURA = r'\\inicio|\\INICIO'
 t_FINAL_PARTITURA = r'\\fin|\\FIN'
-# t_REDONDA = r'r'
-# t_BLANCA = r'b'
-# t_NEGRA = r'n'
-# t_CORCHEA = r'c'
-# t_SEMICORCHEA = r's'
-# t_FUSA = r'f'
-# t_SEMIFUSA = r'sf'
-# t_SILENCIO_REDONDA = r'sir'
-# t_SILENCIO_BLANCA = r'sib'
-# t_SILENCIO_NEGRA = r'sin'
-# t_SILENCIO_CORCHEA = r'sic'
-# t_SILENCIO_SEMICORCHEA = r'sis'
-# t_SILENCIO_FUSA = r'sif'
-# t_SILENCIO_SEMIFUSA = r'sisf'
 t_DIVISOR_TEMPO = r'/'
 t_PARENTESIS_ABIERTO = r'\('
 t_PARENTESIS_CERRADO = r'\)'
@@ -145,130 +73,6 @@ t_LLAVE_ABIERTA = r'\{'
 t_LLAVE_CERRADA = r'\}'
 t_SEPARACION_COMPAS = r','
 t_POTENCIA_CLAVE = r'\^'
-# t_SUMA = r'\+'
-# t_RESTA = r'-'
-# t_MINUSMINUS = r'\-\-'
-# # t_PUNTO = r'\.'
-# t_MULT = r'\*'
-# t_DIV = r'/'
-# t_MODULO = r'\%'
-# t_POTENCIA = r'(\*{2} | \^)'
-
-# t_ASIGNAR = r'='
-# # Expresiones Logicas
-# t_AND = r'\&\&'
-# t_OR = r'\|{2}'
-# t_NOT = r'\!'
-# t_MENORQUE = r'<'
-# t_MAYORQUE = r'>'
-# t_PUNTOCOMA = ';'
-# t_COMA = r','
-# t_PARIZQ = r'\('
-# t_PARDER = r'\)'
-# t_CORIZQ = r'\['
-# t_CORDER = r'\]'
-# t_LLAIZQ = r'{'
-# t_LLADER = r'}'
-# t_COMDOB = r'\"'
-
-
-# def t_INCLUDE(t):
-#     r'include'
-#     return t
-
-# def t_USING(t):
-#     r'using'
-#     return t
-
-# def t_NAMESPACE(t):
-#     r'namespace'
-#     return t
-
-# def t_STD(t):
-#     r'std'
-#     return t
-
-# def t_COUT(t):
-#     r'cout'
-#     return t
-
-# def t_CIN(t):
-#     r'cin'
-#     return t
-
-# def t_GET(t):
-#     r'get'
-#     return t
-
-# def t_ENDL(t):
-#     r'endl'
-#     return t
-
-# def t_SINO(t):
-#     r'else'
-#     return t
-
-# def t_SI(t):
-#     r'if'
-#     return t
-
-# def t_RETURN(t):
-#    r'return'
-#    return t
-
-# def t_VOID(t):
-#    r'void'
-#    return t
-
-# def t_MIENTRAS(t):
-#     r'while'
-#     return t
-
-# def t_PARA(t):
-#     r'for'
-#     return t
-
-# def t_ENTERO(t):
-#     r'\d+'
-#     t.value = int(t.value)
-#     return t
-
-
-# def t_CADENA(t):
-#    r'\"?(\w+ \ *\w*\d* \ *)\"?'
-#    return t
-
-# def t_NUMERAL(t):
-#     r'\#'
-#     return t
-
-# def t_PLUSPLUS(t):
-#     r'\+\+'
-#     return t
-
-# def t_MENORIGUAL(t):
-#     r'<='
-#     return t
-
-# def t_MAYORIGUAL(t):
-#     r'>='
-#     return t
-
-# def t_IGUAL(t):
-#     r'=='
-#     return t
-
-# def t_MAYORDER(t):
-#     r'<<'
-#     return t
-
-# def t_MAYORIZQ(t):
-#     r'>>'
-#     return t
-
-# def t_DISTINTO(t):
-#     r'!='
-#     return t
 
 def t_INICIO(t):
     r'inicio|INICIO'
